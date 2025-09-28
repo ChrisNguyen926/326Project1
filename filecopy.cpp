f96289b2e155aee1769ae16809eb886249dfcdc6
AjMeade <66539621+AkeemMeade@users.noreply.github.com>
2025-09-28 16:48:19 -0700

    filecopy

---
+++ filecopy.cpp
@@ -0,0 +1,81 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+#define READ_END 0
+#define WRITE_END 1
+
+int main(int argc, char *argv[]) {
+  char buffer[4096];
+  int fd[2];
+  pid_t pid;
+
+  if (argc < 3) {
+    std::cerr << "Enter: ./filecopy [source] [destination]" << std::endl;
+    exit(1);
+  }
+
+  std::string src(argv[1]);
+  std::string dst(argv[2]);
+
+  // std::cout << "source file: " << src << "\n" << "destination file: " << dest << std::endl;
+  if (pipe(fd) == -1) {
+    perror("pipe failed");
+    exit(1);
+  }
+
+  // set the fork return value to the process ID, 0 = child, 1 = parent
+  pid = fork();
+
+  if (pid < 0) {
+    // fork fails
+
+    perror("fork failed");
+    exit(1);
+  }
+  else if (pid > 0) {
+    // parent
+
+    std::ifstream inFile(src);
+    if (!inFile.is_open()) {
+      std::cerr << "Failed to open source file" << std::endl;
+      exit(1);
+    }
+
+    close(fd[READ_END]);
+
+    while (inFile.read(buffer, sizeof(buffer)) || inFile.gcount() > 0) {
+      ssize_t n = inFile.gcount();
+      write(fd[WRITE_END], buffer, inFile.gcount());
+    }
+
+    inFile.close();
+    close(fd[WRITE_END]);
+  }
+  else {
+    // Child
+
+    std::ofstream outFile(dst);
+    if (!outFile.is_open()) {
+      exit(1);
+    }
+
+    close(fd[WRITE_END]);
+    ssize_t n;
+
+    while ((n = read(fd[READ_END], buffer, sizeof(buffer))) > 0) {
+      outFile.write(buffer, n);
+    }
+
+    std::cout << "File successfully copied from " << src << "  to " << dst << std::endl;
+
+    outFile.close();
+    close(fd[READ_END]);
+  }
+
+  return 0;
+}
